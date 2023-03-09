a4bf7cbe11f9807c076de72306b77c9243519e93
Abdullah Ciftci <abdullahciftci1515@gmail.com>
2023-03-09 18:04:51 +0100

    Projektdateien hinzufügen.

---
+++ Matrix Bildschirm/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Matrix_Bildschirm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Textfarbe einstellen
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            //Random Nummer
+            Random rand = new Random();
+
+            String str = "";
+
+            Console.Write("Drücken Sie zum Starten Enter.... ");
+            Console.ReadKey();
+
+            for (int i = 0; i < 20000; i++)
+            {
+
+                if(i % 2 == 0)
+                {
+                    str = "";
+                    for (int j = 0; j < 79; j++)
+                    {
+                        int n = rand.Next(5);
+                        if (n < 2)
+                        {
+                            str += n.ToString();
+                        }
+                        else
+                        {
+                            str += " ";
+                        }
+                    }
+                }
+
+
+                Console.WriteLine(str);
+            }
+
+            Console.WriteLine("Ende");
+            Console.WriteLine("Bye");
+            Console.ReadKey();
+        }
+    }
+}
